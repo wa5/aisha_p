4cb29a7489ace12d0352df7cf9669dab945fbbf7
wa5 <waseemslyar@gmail.com>
2022-10-30 15:10:15 +0530

    kk

---
+++ buyerSeller-main/client/src/app/components/productsuploaded/component/ProductsList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+interface IProductsList{}
+
+export const  ProductsList:React.FC<IProductsList>=(props) =>{
+  return (<>
+  <div> ProductsList</div>
+  </>
+    
+  )
+}
